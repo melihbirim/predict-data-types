4fad92f2c6a543334b4c6d4c2b3e94ec97ccaac7
Melih Birim <melihbirim@gmail.com>
2025-12-20 22:54:43 +0300

    chore: remove test file for lint hook demo

--- test-lint-hook.js
+++
@@ -1,9 +0,0 @@
-// Test file with intentional but auto-fixable lint errors
-const test = 'double quotes instead of single';
-console.log(test);
-const   spacing='bad spacing';
-console.log(spacing);
-const trailing = 'has trailing spaces';
-console.log(trailing);
-const semicolon = 'missing semicolon';
-console.log(semicolon);
