4ab3594f1283a405ea3cf3fb4cdd10bc27227fc7
Melih Birim <melihbirim@gmail.com>
2025-12-20 22:54:27 +0300

    test: verify pre-commit hook auto-fixes linting

---
+++ test-lint-hook.js
@@ -0,0 +1,9 @@
+// Test file with intentional but auto-fixable lint errors
+const test = 'double quotes instead of single';
+console.log(test);
+const   spacing='bad spacing';
+console.log(spacing);
+const trailing = 'has trailing spaces';
+console.log(trailing);
+const semicolon = 'missing semicolon';
+console.log(semicolon);
